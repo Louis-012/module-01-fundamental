a121b05dd9caad7700142d5e078aadd09d9cc110
Louis-012 <lpanglewai12@gmail.com>
2025-06-02 14:50:05 +0700

    new exercise

--- day03/exercise.ts
+++ day03/exercise.ts
@@ -1,1 +1,63 @@
-// exercise 01
+// Write a code to check whether the number is odd or even
+// ○ Example: 25 → odd number, 2 → even number
+
+let number: number = 13;
+
+if (number % 2 == 0) {
+  const print: string = `${number} : even number`;
+  console.log(print);
+} else if (number % 2 == 1) {
+  const print: string = `${number} : odd number`;
+  console.log(print);
+}
+
+// Write a code to check whether the number is prime number or not
+// ○ Example: 7 → 7 is a prime number
+// ○ Example: 6 → 6 is not a prime number
+
+number = 7;
+let isPrime: boolean = true;
+for (let index = 2; index < number; index++) {
+  if (number % index === 0) {
+    isPrime = false;
+    break;
+  }
+}
+console.log(isPrime);
+
+//Write a code to find the sum of the numbers 1 to N
+// ○ Example: 5 → 1 + 2 + 3 + 4 + 5 = 15
+// ○ Example: 3 → 1 + 2 + 3 = 6
+
+let limit: number = 3;
+let total: number = 0;
+for (let index = 1; index <= limit; index++) {
+  total += index;
+}
+console.log(total);
+
+// Write a code to find factorial of a number
+// ○ Example: 4! → 4 x 3 x 2 x 1 = 24
+// ○ Example: 6! → 6 x 5 x 4 x 3 x 2 x 1 = 720
+
+limit = 6;
+total = 1;
+for (let index = 2; index <= limit; index++) {
+  total *= index;
+}
+console.log(total);
+
+//Write a code to print the first N fibonacci numbers
+// ○ Example: 15 → 610
+
+limit = 15;
+let n1: number = 0;
+let n2: number = 1;
+let fib: number;
+
+for (let index = 0; index <= limit; index++) {
+  console.log(n1);
+  fib = n1 + n2;
+  n1 = n2;
+  n2 = fib;
+}
